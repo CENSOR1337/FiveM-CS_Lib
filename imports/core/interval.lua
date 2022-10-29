e02a073a0a62642d3173e929d9b6303f857b9e25
CENSOR_1337 <viixxivvimm@gmail.com>
2022-10-29 18:50:59 +0700

    feat: interval

--- imports/core/interval.lua
+++ imports/core/interval.lua
@@ -1,44 +1,41 @@
-do
-    local Interval = {}
-    Interval.__index = Interval
-    local _wait = Wait
-    function Interval.new(handler, delay, options)
-        local self = {}
-        self.handler = handler
-        self.delay = delay
-        self.bDestroyed = false
-        self.bLoop = (options.bLoop ~= nil) or false
-        Citizen.CreateThreadNow(function(ref)
-            self.id = ref
-            if (self.bLoop) then
-                while not (self.bDestroyed) do
-                    _wait(self.delay)
-                    if (self.bDestroyed) then break end
-                    self.handler()
-                end
-            else
+local Interval = {}
+Interval.__index = Interval
+local _wait = Wait
+function Interval.new(handler, delay, options)
+    local self = {}
+    self.handler = handler
+    self.delay = delay or 0
+    self.bDestroyed = false
+    self.bLoop = (options.bLoop ~= nil) and options.bLoop or false
+    Citizen.CreateThreadNow(function(ref)
+        self.id = ref
+        if (self.bLoop) then
+            while not (self.bDestroyed) do
                 _wait(self.delay)
-                if (self.bDestroyed) then return end
+                if (self.bDestroyed) then break end
                 self.handler()
             end
-        end)
-        return setmetatable(self, Interval)
-    end
+        else
+            _wait(self.delay)
+            if (self.bDestroyed) then return end
+            self.handler()
+        end
+    end)
+    return setmetatable(self, Interval)
+end
 
-    function Interval:destroy()
-        self.bDestroyed = true
-    end
+function Interval:destroy()
+    self.bDestroyed = true
+end
 
-    function self.setInterval(handler, interval)
-        return Interval.new(handler, interval, { bLoop = true })
-    end
+function self.setInterval(handler, interval)
+    return Interval.new(handler, interval, { bLoop = true })
+end
 
-    function self.setTimeout(handler, interval)
-        return Interval.new(handler, interval, { bLoop = false })
-    end
+function self.setTimeout(handler, interval)
+    return Interval.new(handler, interval, { bLoop = false })
+end
 
-    function self.clearInterval(interval)
-        interval:destroy()
-    end
-
+function self.clearInterval(interval)
+    interval:destroy()
 end
