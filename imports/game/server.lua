12735fe9ad6412a33c4fc6acd08f9dea474b1cf8
CENSOR_1337 <viixxivvimm@gmail.com>
2022-11-22 01:36:21 +0700

    Chore(Game): Added server sided functions

--- imports/game/server.lua
+++ imports/game/server.lua
@@ -1,3 +1,40 @@
-function self.getEntities()
+function self.getObjects()
     return GetAllObjects()
+end
+
+function self.getPeds()
+    return GetAllPeds()
+end
+
+function self.getVehicles()
+    return GetAllVehicles()
+end
+
+function self.getEntitiesByTypes(types)
+    local entities = {}
+    local count = 0
+
+    for i = 1, #types, 1 do
+        local poolType = types[i]
+        local pool = {}
+
+        if poolType == "CObject" then
+            pool = self.getObjects()
+        end
+        
+        if poolType == "CPed" then
+            pool = self.getPeds()
+        end
+        
+        if poolType == "CVehicle" then
+            pool = self.getVehicles()
+        end
+
+        for i = 1, #pool, 1 do
+            count += 1
+            entities[count] = pool[i]
+        end
+    end
+
+    return entities
 end
