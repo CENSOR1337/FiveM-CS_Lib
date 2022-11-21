be0eb1aa8a3ac734d888be2e51d1197d1e1c6dbd
CENSOR_1337 <viixxivvimm@gmail.com>
2022-11-21 18:13:41 +0700

    init(Zone): added zone component

---
+++ imports/zone/client.lua
@@ -0,0 +1,16 @@
+self.sphere = setmetatable({
+    new = function(coords, radius, options)
+        local collision = cslib.collision.createSphere({
+            position = vec(coords.x, coords.y, coords.z),
+            radius = radius,
+            bDebug = options.bDebug,
+            bOnlyRelevant = true
+        })
+        collision:addRelevantPlayer(PlayerId())
+        return sphere
+    end
+}, {
+    __call = function(t, ...)
+        return t.new(...)
+    end
+})
