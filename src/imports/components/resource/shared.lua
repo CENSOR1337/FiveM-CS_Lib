9bafd009e1e64af2e821a434a3c99ebca98ca832
CENSOR_1337 <viixxivvimm@gmail.com>
2023-02-28 01:26:51 +0700

    feat: add onStart, onStop, and general event handler to resource component

--- src/imports/components/resource/shared.lua
+++ src/imports/components/resource/shared.lua
@@ -1,9 +1,43 @@
 local resourceName = GetCurrentResourceName()
+local getResourceEventName = function(eventname)
+    return resourceName .. ":" .. eventname
+end
+
 return {
     name = resourceName,
     event = setmetatable({}, {
         __call = function(t, eventname)
-            return resourceName .. ":" .. eventname
+            return getResourceEventName(eventname)
         end
-    })
+    }),
+    onStop = function(cb)
+        lib.on("onResourceStop", function(resource)
+            if resource ~= resourceName then return end
+            cb(resource)
+        end)
+    end,
+    onStart = function(cb)
+        lib.on("onResourceStart", function(resource)
+            if resource ~= resourceName then return end
+            cb(resource)
+        end)
+    end,
+    on = function(eventname, cb)
+        lib.on(getResourceEventName(eventname), cb)
+    end,
+    onNet = function(eventname, cb)
+        lib.onNet(getResourceEventName(eventname), cb)
+    end,
+    emit = function(eventname, ...)
+        lib.emit(getResourceEventName(eventname), ...)
+    end,
+    emitServer = (not lib.bIsServer) and function(eventname, ...)
+        lib.emitServer(getResourceEventName(eventname), ...)
+    end,
+    emitClient = (lib.bIsServer) and function(eventname, target, ...)
+        lib.emitClient(getResourceEventName(eventname), target, ...)
+    end,
+    emitAllClients = (lib.bIsServer) and function(eventname, ...)
+        lib.emitAllClients(getResourceEventName(eventname), -1, ...)
+    end,
 }
