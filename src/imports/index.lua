e728a5fc765c0a83cefcdc2d0ca854bb0b762cf0
CENSOR_1337 <viixxivvimm@gmail.com>
2023-03-02 15:34:58 +0700

    feat: throw an error if lua54 was not enabled

--- src/imports/index.lua
+++ src/imports/index.lua
@@ -1,3 +1,6 @@
+--[[ Init ]]
+if (not _VERSION:find("5.4")) then error("^1[ Please enable Lua 5.4 ]^0", 2) end
+
 local isServer = IsDuplicityVersion()
 
 local function bindOnce(bIsNet, eventname, listener)
