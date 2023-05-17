7cc55a2d609a907cd3088e394af342bccc071ed9
CENSOR_1337 <viixxivvimm@gmail.com>
2023-05-17 21:08:49 +0700

    refactor: change way to assemble component

--- src/imports/components/network/shared.lua
+++ src/imports/components/network/shared.lua
@@ -2,11 +2,9 @@
     `network` module will be removed in the future
     Use `net` module instead
  ]]
-local library = {}
-if (lib.isServer) then
-    library.registerServerCallback = lib.net.callback.register
+if (lib.bIsServer) then
+    cslib_component.registerServerCallback = lib.net.callback.register
 else
-    library.triggerServerCallback = lib.net.callback
-    library.triggerServerCallbackSync = lib.net.callback.await
+    cslib_component.triggerServerCallback = lib.net.callback
+    cslib_component.triggerServerCallbackSync = lib.net.callback.await
 end
-return library
