ff21493f0aad721243403cb555b678da5b42d27d
CENSOR_1337 <viixxivvimm@gmail.com>
2023-12-23 14:38:40 +0700

    feat: add utility set class

---
+++ src/imports/components/set/shared.lua
@@ -0,0 +1,71 @@
+local set = {}
+set.__index = set
+
+function set.new()
+    local self = {}
+    self.data = {}
+    self.length = 0
+    
+    return setmetatable(self, set)
+end
+
+function set:contain(value)
+    return self.data[value] ~= nil
+end
+
+function set:contains(...)
+    local args = { ... }
+
+    for i = 1, #args do
+        if not (self:contain(args[i])) then return false end
+    end
+
+    return true
+end
+
+function set:append(...)
+    local args = { ... }
+    for i = 1, #args do
+        local otherSet = args[i]
+        lib.typeCheck(otherSet, "table")
+        lib.typeCheck(otherSet.data, "table")
+
+        for value in pairs(otherSet.data) do
+            self:add(value)
+        end
+    end
+end
+
+function set:array()
+    local array = {}
+    for value in pairs(self.data) do
+        array[#array + 1] = value
+    end
+    return array
+end
+
+function set:add(value)
+    lib.typeCheck(value, "string", "number", "boolean", "table")
+
+    self.data[value] = true
+    self.length = self.length + 1
+end
+
+function set:remove(value)
+    lib.typeCheck(value, "string", "number", "boolean", "table")
+    if not (self:contain(value)) then return end
+    
+    self.data[value] = nil
+    self.length = self.length - 1
+end
+
+function set:count()
+    return self.length
+end
+
+function set:clear()
+    self.data = {}
+    self.length = 0
+end
+
+cslib_component.set = set
