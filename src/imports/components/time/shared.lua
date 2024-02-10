7a0955cdf5cc0ffac5a42c9e19b94dbd072e70d8
Metee Tanathip <censor31337@gmail.com>
2024-02-10 17:50:20 +0700

    feat: add timeSinceNet

--- src/imports/components/time/shared.lua
+++ src/imports/components/time/shared.lua
@@ -4,5 +4,10 @@ local function TimeSince(time)
     return GetGameTimer() - time
 end
 
+local function timeSinceNet(time)
+    return cslib_component.getNetTime() - time
+end
+
 cslib_component.getTime = GetGameTimer
 cslib_component.timeSince = TimeSince
+cslib_component.timeSinceNet = timeSinceNet
