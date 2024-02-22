d92ea192d814d2af3f3edce2c3959538ae6249a1
Metee Tanathip <censor31337@gmail.com>
2024-02-22 19:36:00 +0700

    refactor:
     - add `playerped` into type
    
     - fix using wrong set function
    
     - now just dispatch to listener with each entity

--- src/imports/components/entityStreamer/client.lua
+++ src/imports/components/entityStreamer/client.lua
@@ -1,6 +1,20 @@
 local EnityStreamer = {}
 EnityStreamer.__index = EnityStreamer
 
+local function getNonPlayerPeds()
+    local peds = lib.game.getPeds()
+    local aiPeds = {}
+    local count = 0
+    for i = 1, #peds, 1 do
+        local ped = peds[i]
+        if (not lib.game.isPedAPlayer(ped)) then
+            count += 1
+            aiPeds[count] = ped
+        end
+    end
+    return aiPeds
+end
+
 function EnityStreamer.new(options)
     lib.typeCheck(options, "table", "nil")
 
@@ -8,7 +22,7 @@ function EnityStreamer.new(options)
     options.types = options.types or { "ped", "vehicle", "object", "playerped" }
 
     local self = setmetatable({}, EnityStreamer)
-    self.types = lib.fromArray(options.types)
+    self.types = lib.set.fromArray(options.types)
     self.tickRate = options.tickRate or 500
     self.tickpool = lib.tickpool.new()
     self.dispatcher = lib.dispatcher.new()
@@ -16,19 +30,22 @@ function EnityStreamer.new(options)
     self.tickpool:onTick(function()
         local entities = {}
 
-        if (self.types:contains("object")) then
+        if (self.types:contain("object")) then
             entities["object"] = lib.game.getObjects()
         end
 
-        if (self.types:contains("ped")) then
-            entities["ped"] = lib.game.getPeds()
+        if (self.types:contain("ped")) then
+            entities["ped"] = getNonPlayerPeds()
         end
 
-        if (self.types:contains("vehicle")) then
+        if (self.types:contain("vehicle")) then
             entities["vehicle"] = lib.game.getVehicles()
         end
 
-        local procressedEntities = {}
+        if (self.types:contain("playerped")) then
+            entities["playerped"] = lib.game.getPlayerPeds()
+        end
+
         for entityType, entityHandles in pairs(entities) do
             for i = 1, #entityHandles, 1 do
                 local entityHandle = entityHandles[i]
@@ -37,18 +54,17 @@ function EnityStreamer.new(options)
                 if (cslib.isServer) then
                     dimension = GetEntityRoutingBucket(entityHandle)
                 end
+
                 local processedEntity = {
-                    type = entityType,
-                    pos = GetEntityCoords(entityHandle),
+                    entityType = entityType,
+                    position = GetEntityCoords(entityHandle),
                     handle = entityHandle,
                     dimension = dimension,
                 }
 
-                procressedEntities[#procressedEntities + 1] = processedEntity
+                self.dispatcher:broadcast(processedEntity)
             end
         end
-
-        self.dispatcher:broadcast(procressedEntities)
     end)
 
     return self
@@ -58,3 +74,11 @@ function EnityStreamer:onTick(listener)
     lib.typeCheck(listener, "function", "table")
     return self.dispatcher:add(listener)
 end
+
+cslib_component = setmetatable({
+    new = EnityStreamer.new,
+}, {
+    __call = function()
+        return EnityStreamer.new()
+    end,
+})
