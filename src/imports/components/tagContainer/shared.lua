717d792b459538fdd50715c97ab3e1113bd22b64
CENSOR37 <viixxivvimm@gmail.com>
2025-04-14 07:48:06 +0700

    Update shared.lua

--- src/imports/components/tagContainer/shared.lua
+++ src/imports/components/tagContainer/shared.lua
@@ -32,6 +32,39 @@ function TagContainer:clear()
     table.wipe(self.tags)
 end
 
+local ReplicatedTagContainer = {}
+ReplicatedTagContainer.__index = ReplicatedTagContainer
+ReplicatedTagContainer.__eventname = {
+    onTagContainerUpdate = "on.tag.container.update",
+}
+
+function ReplicatedTagContainer.new(id, ...)
+    local self = {}
+    self.id = id
+    self.container = TagContainer.new(...)
+    self.events = {
+        onTagContainerUpdate = ("%s:%s"):format(joaat("tag:rep:get:tags"), id),
+    }
+
+    if (lib.isServer) then
+        lib.resource.callback.register(self.events.onTagContainerUpdate, function()
+            return self.container.tags
+        end)
+    else
+
+    end
+
+    return setmetatable(self, ReplicatedTagContainer)
+end
+
+function ReplicatedTagContainer:updateTag()
+    if (lib.isServer) then
+        lib.resource.emitAllClients(self.events.onTagContainerUpdate)
+    end
+    local tags = lib.resource.callback.await(self.events.onTagContainerUpdate)
+    local newTagContainer = TagContainer.new()
+end
+
 cslib_component = setmetatable({
     new = TagContainer.new,
 }, {
